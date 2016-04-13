7678bf2771d79342c38ecd46fe0db118214d5aaf
David Sudia <dsudia@gmail.com>
2016-04-13 16:52:44 -0600

    gif details are displayed on /gif

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,11 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Created by davidsudia on 4/12/16.
@@ -16,8 +19,9 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String singleGif() {
-        return "This would be a GIF if I knew what I was doing.";
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("android-explosion", LocalDate.of(2015, 2, 13), "Dave Sudia", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
